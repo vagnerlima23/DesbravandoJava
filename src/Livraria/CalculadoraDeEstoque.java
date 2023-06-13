43dbb79c6349a972a1aa272617391963f9c4e0b7
“vagnerlima23”1~ <vagner_lima23@hotmail.com>
2023-06-13 20:29:22 -0300

    looping

--- src/Livraria/CalculadoraDeEstoque.java
+++ src/Livraria/CalculadoraDeEstoque.java
@@ -1,22 +1,28 @@
 package Livraria;
 
+//2.6 loopings
+
 public class CalculadoraDeEstoque {
 	public static void main (String[]args) {
-		double livroJava8 = 59.90;
-		double livroTDD = 59.90;
 		
-		double soma = livroJava8 + livroTDD;
+		double soma = 0;
+		int contador = 0;
+		
+		while (contador < 35) {
+			double valorDoLivro = 59.90;
+			soma = soma + valorDoLivro;
+			contador = contador + 1;
+		}
 		System.out.println("O total em estoque é " + soma);
 		
-		if(soma < 150) {
-			System.out.println("Seu estoque está muito baixo !");
-		}else if (soma >= 2000){
-			System.out.println("Seu estoque está muito alto");
-		}else {
-			System.out.println("Seu estoque está bom");
-		}
-		System.out.println("QUE LEGAL CARA ");
+		if (soma < 150){
+		System.out.println("Seu estoque está muito baixo");
+	} else if(soma >= 2000) {
+		System.out.println("Seu estoque está muito alto");
 		
+	} else {
+		System.out.println("Seu estoque está bom");
 	}
-
+		}
 }
+	
